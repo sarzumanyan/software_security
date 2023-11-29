563a7fdc2e444d2731611959f04ca78fb81538ad
Sona Arzumanyan <sona.arzumanyan42@gmail.com>
2023-11-30 01:12:39 +0400

    Added general version of Vigenere's cipher

--- vigenere.cpp
+++ vigenere.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 using namespace std;
- 
-// This function generates the key in
-// a cyclic manner until it's length isn't
-// equal to the length of original text
+
 string generateKey(string str, string key)
 {
     int x = str.size();
- 
+
     for (int i = 0; ; i++)
     {
         if (x == i)
@@ -18,34 +15,32 @@ string generateKey(string str, string key)
     }
     return key;
 }
- 
-// This function returns the encrypted text
-// generated with the help of the key
+
 string cipherText(string str, string key)
 {
     string cipher_text;
- 
+
     for (int i = 0; i < str.size(); i++)
     {
-        // converting in range 0-25
-        char x = (str[i] + key[i]) %26;
- 
+        // converting in range 0-25 or 0-127
+        char x = (str[i] + key[i]) % 26;
+        //char x = (str[i] + key[i]) % 128;
+
         // convert into alphabets(ASCII)
         x += 'A';
- 
+
         cipher_text.push_back(x);
     }
     return cipher_text;
 }
 int main()
 {
-    string str = "GEEKSFORGEEKS";
+    string str = "BANANATREE";
     string keyword = "LEMON";
- 
+
     string key = generateKey(str, keyword);
     string cipher_text = cipherText(str, key);
- 
-    cout << "Ciphertext : "
-         << cipher_text << "\n";
+
+    cout << "Ciphertext : " << cipher_text << "\n";
     return 0;
 }
